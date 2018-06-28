65634e97ae25907824c63f9e05f94e0f45721c64
julien hacc <julienhacc@gmail.com>
2018-06-28 21:27:53 +0200

    maj call to api in basic auth

--- src/main/java/fr/zenika/search/zenikaresume/backend/parsing/ImportRemoteDataFromApiService.java
+++ src/main/java/fr/zenika/search/zenikaresume/backend/parsing/ImportRemoteDataFromApiService.java
@@ -1,10 +1,16 @@
 package fr.zenika.search.zenikaresume.backend.parsing;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,17 +21,31 @@ import java.util.stream.Collectors;
 @Service
 public class ImportRemoteDataFromApiService {
 
+
     @Autowired
     private ParsingService parsingService;
 
     public List<ParsedUser> fetchDatas()  {
-
         final String uri = System.getenv("zenika.writing.resume.url")+"/resumes/complete";
-        RestTemplate restTemplate = new RestTemplate();
+
+        RestTemplate rt = new RestTemplate();
+        rt.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+        rt.getMessageConverters().add(new StringHttpMessageConverter());
+
+        String plainCreds = System.getenv("USER_AUTH_API_USERNAME")+":"+System.getenv("USER_AUTH_API_PASSWORD");
+        byte[] plainCredsBytes = plainCreds.getBytes();
+        byte[] base64CredsBytes = Base64.encodeBase64(plainCredsBytes);
+        String base64Creds = new String(base64CredsBytes);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Basic " + base64Creds);
+
+        HttpEntity<String> request = new HttpEntity<>(headers);
+
 
         ResponseEntity<List<ResumeApiResponse>> rateResponse =
-                restTemplate.exchange(uri,
-                        HttpMethod.GET, null, new ParameterizedTypeReference<List<ResumeApiResponse>>() {
+                    rt.exchange(uri,
+                        HttpMethod.GET, request, new ParameterizedTypeReference<List<ResumeApiResponse>>() {
                         });
         List<ResumeApiResponse> resumeApiResponses = rateResponse.getBody().stream()
                 .filter(resumeApiResponse -> StringUtils.isNotEmpty(resumeApiResponse.getPath())).collect(Collectors.toList());

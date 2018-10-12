5e7ecf30e95550ff4fa1a8e0316c26278bd66d4e
Hugo Wood <hx4.5@free.fr>
2018-10-12 13:08:06 +0200

    clean up

--- src/main/java/fr/zenika/search/zenikaresume/backend/ImportResumeJobMain.java
+++ src/main/java/fr/zenika/search/zenikaresume/backend/ImportResumeJobMain.java
@@ -1,15 +1,18 @@
 package fr.zenika.search.zenikaresume.backend;
 import org.springframework.beans.factory.annotation.Autowired;
 import javax.annotation.PostConstruct;
+
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 import org.springframework.boot.SpringApplication;
 import fr.zenika.search.zenikaresume.backend.parsing.ElasticsearchUserService;
 import fr.zenika.search.zenikaresume.backend.parsing.ImportRemoteDataFromApiService;
 
 @Service
-public class ImportResumeJobMain{
+public class ImportResumeJobMain {
+
     public static void main(String[] args) {
-        SpringApplication.run(ZenikaSearchResumeApplication.class, args);        
+        SpringApplication.run(ZenikaSearchResumeApplication.class, args);
     }
 
     @Autowired
@@ -21,5 +24,5 @@ public class ImportResumeJobMain{
     @PostConstruct
     private void runImportJob(){
         elasticsearchUserService.indexUsers(importRemoteDataFromApiService.fetchDatas());
-    } 
+    }
 }
